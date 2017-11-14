7ebb2398b31ad73bfc5a98ae6820a4e56f0b02c9
Brian Gallagher <brian.gallagher@feedhenry.com>
2017-11-14 16:04:26 +0000

    update App.js

--- environments/local.js
+++
@@ -1,7 +0,0 @@
-exports.get = function () {
-  return {
-    'env' : {
-      'API_URL': 'https://localhost:8001'
-    }
-  }
-}
--- src/App.js
+++ src/App.js
@@ -23,82 +23,41 @@ import DragulaDemo from './components/DragulaDemo';
 import { getInitConfig } from './actions/action_init';
 import thunk from 'redux-thunk';
 
-// const createStoreWithMiddleware = applyMiddleware(promise, thunk)(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise, thunk)(createStore);
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      // store:createStoreWithMiddleware(reducers)
-    }
-  }
-
-
-  componentDidMount() {
-    console.log('Component did mount');
-    // debugger;
-    // this.props.getInitConfig();
-  }
 
   render() {
 
-    // console.log(process.env);
-    // console.log(process.env);
-    // console.log(process.env);
-    // console.log(process.env);
+    console.log(process.env);
+    console.log(process.env);
+    console.log(process.env);
+    console.log(process.env);
 
     return (
-      <BrowserRouter>
-          <Switch>
-            <Route path="/dragula" component={DragulaDemo} />
-            <Route path="/engagements" component={Engagements} />
-            <Route path="/dashboard/:id" component={Dashboard} />
-            <Route path="/procharter/:engagementId" component={ProCharter} />
-            <Route path="/rti/:id" component={RTI} />
-            <Route path="/mindmap/:id" component={MindMap} />
-            <Route path="/archreview/:id" component={ArchReview} />
-            <Route path="/businessreview/:id" component={BusinessReview} />
-            <Route path="/workshops/:engagementId" component={WorkShops} />
-            <Route path="/userStories/:engagementId/:workshopName" component={UserStories} />
-            <Route path="/userStory/:configId/:usId" component={UserStory} />
-            <Route path="/hlarch/:id" component={HlArch} />
-            <Route path="/nextsteps/:id" component={NextSteps} />
-            <Route path="/login" component={Login} />
-            <Route path="/" component={Login} />
-          </Switch>
-      </BrowserRouter>
+      <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <Switch>
+              <Route path="/dragula" component={DragulaDemo} />
+              <Route path="/engagements" component={Engagements} />
+              <Route path="/dashboard/:id" component={Dashboard} />
+              <Route path="/procharter/:engagementId" component={ProCharter} />
+              <Route path="/rti/:id" component={RTI} />
+              <Route path="/mindmap/:id" component={MindMap} />
+              <Route path="/archreview/:id" component={ArchReview} />
+              <Route path="/businessreview/:id" component={BusinessReview} />
+              <Route path="/workshops/:engagementId" component={WorkShops} />
+              <Route path="/userStories/:engagementId/:workshopName" component={UserStories} />
+              <Route path="/userStory/:configId/:usId" component={UserStory} />
+              <Route path="/hlarch/:id" component={HlArch} />
+              <Route path="/nextsteps/:id" component={NextSteps} />
+              <Route path="/login" component={Login} />
+              <Route path="/" component={Login} />
+            </Switch>
+        </BrowserRouter>
+      </Provider>
     );
-
-    // return (
-    //   <Provider store={createStoreWithMiddleware(reducers)}>
-    //     <BrowserRouter>
-    //         <Switch>
-    //           <Route path="/dragula" component={DragulaDemo} />
-    //           <Route path="/engagements" component={Engagements} />
-    //           <Route path="/dashboard/:id" component={Dashboard} />
-    //           <Route path="/procharter/:engagementId" component={ProCharter} />
-    //           <Route path="/rti/:id" component={RTI} />
-    //           <Route path="/mindmap/:id" component={MindMap} />
-    //           <Route path="/archreview/:id" component={ArchReview} />
-    //           <Route path="/businessreview/:id" component={BusinessReview} />
-    //           <Route path="/workshops/:engagementId" component={WorkShops} />
-    //           <Route path="/userStories/:engagementId/:workshopName" component={UserStories} />
-    //           <Route path="/userStory/:configId/:usId" component={UserStory} />
-    //           <Route path="/hlarch/:id" component={HlArch} />
-    //           <Route path="/nextsteps/:id" component={NextSteps} />
-    //           <Route path="/login" component={Login} />
-    //           <Route path="/" component={Login} />
-    //         </Switch>
-    //     </BrowserRouter>
-    //   </Provider>
-    // );
   }
 }
 
-function mapStateToProps (state) {
-  return { config: state.config };
-}
-
-export default connect(mapStateToProps, { getInitConfig })(App);
-
-// export default App;
+export default App;
