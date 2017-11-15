2193afcedd5beafd1807bcaf869ece704307affb
Brian Gallagher <brian.gallagher@feedhenry.com>
2017-11-15 14:29:17 +0000

    updatE

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(promise, thunk)(createStore);
+ReactDOM.render(<Provider store={createStoreWithMiddleware(reducers)}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
+
+
+
